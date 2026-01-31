f0576f84dc6ea57b96c0e2e504f2c3c37377b4de
Lakshita Bhardwaj <lakshitabhardwaj321@gmail.com>
2026-01-31 20:36:28 +0530

    Added Day 7

---
+++ Week-01-Arrays/ProductExceptSelf.java
@@ -0,0 +1,30 @@
+public class ProductExceptSelf {
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        // Step 1: Prefix products
+        ans[0] = 1;
+        for (int i = 1; i < n; i++) {
+            ans[i] = ans[i - 1] * nums[i - 1];
+        }
+
+        // Step 2: Suffix products
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            ans[i] = ans[i] * suffix;
+            suffix = suffix * nums[i];
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        int[] result = productExceptSelf(nums);
+
+        for (int val : result) {
+            System.out.print(val + " ");
+        }
+    }
+}
